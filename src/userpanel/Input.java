5c7e45afdd7091376b4a11ce26f532e024eebc0b
MarkisJr <noahdj6@gmail.com>
2020-08-01 10:15:12 +1000

    demo

--- src/userpanel/Input.java
+++ src/userpanel/Input.java
@@ -10,51 +10,57 @@ import java.awt.*;
  *  Made by MarkisJr. 28/07/2020
  */
 
-class Window extends JPanel
+class Window extends JComponent
 {
 	private static final long serialVersionUID = 1L;
 	int temp=0;
 	
-	public void init()
+	public void paint(Graphics g)
 	{
-		JToggleButton pixel[][] = new JToggleButton[24][24];
-		
+		System.out.println("Test");
 		for (int y=0; y<24; y++)
 		{
 			for (int x=0; x<24; x++)
 			{
-				pixel[y][x] = new JToggleButton();
-				pixel[y][x].setPreferredSize(new Dimension(10,10));
-				pixel[y][x].setMargin(new Insets(0,0,0,0));
-				
-				pixel[y][x].setBorder(null);
-				
-				if (y % 2 == 0)
+				temp = x % 2;
+				if (x % 2 == 0)
 				{
-					temp = x % 2;
-					switch (temp) 
+					switch (temp)
 					{
-						case 0: pixel[y][x].setBackground(Color.WHITE);
+						case 0: g.setColor(Color.WHITE);
 						break;
-						case 1: pixel[y][x].setBackground(Color.BLACK);
-						break;
+						case 1: g.setColor(Color.BLACK);
 					}
 				}
-				else 
+				else
 				{
-					temp = x % 2;
-					switch (temp) 
+					switch (temp)
 					{
-						case 0: pixel[y][x].setBackground(Color.BLACK);
+						case 0: g.setColor(Color.BLACK);
 						break;
-						case 1: pixel[y][x].setBackground(Color.WHITE);
-						break;
+						case 1: g.setColor(Color.WHITE);
 					}
 				}
-				add(pixel[y][x]);
+				g.drawRect(x*10, y*10, 10, 10);
 			}
 		}
 	}
+	
+	/*
+	 * public void init() { JToggleButton pixel[][] = new JToggleButton[24][24];
+	 * 
+	 * for (int y=0; y<24; y++) { for (int x=0; x<24; x++) { pixel[y][x] = new
+	 * JToggleButton(); pixel[y][x].setPreferredSize(new Dimension(10,10));
+	 * pixel[y][x].setMargin(new Insets(0,0,0,0));
+	 * 
+	 * pixel[y][x].setBorder(null);
+	 * 
+	 * if (y % 2 == 0) { temp = x % 2; switch (temp) { case 0:
+	 * pixel[y][x].setBackground(Color.WHITE); break; case 1:
+	 * pixel[y][x].setBackground(Color.BLACK); break; } } else { temp = x % 2;
+	 * switch (temp) { case 0: pixel[y][x].setBackground(Color.BLACK); break; case
+	 * 1: pixel[y][x].setBackground(Color.WHITE); break; } } add(pixel[y][x]); } } }
+	 */
 }
 
 
@@ -70,13 +76,13 @@ public class Input
 		 * TrainSet testSet = Mnist.createTrainSet(5000, 9999); Mnist.testTrainSet(net,
 		 * testSet, 10);
 		 */
-		Window importclass = new Window();
-		importclass.init();
+		//Window importclass = new Window();
+		//importclass.init();
 		JFrame w = new JFrame();
 		w.setBounds(200, 200, 246, 269);
 		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		w.setResizable(true);
-		w.getContentPane().add(importclass);
+		w.getContentPane().add(new Window());
 		w.setVisible(true);
 	}
 
