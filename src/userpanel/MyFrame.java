0075a33049f32073c3d12464e264f16cc9dc8cdb
MarkisJr <noahdj6@gmail.com>
2020-08-02 19:03:31 +1000

    foundation of window 50%

--- src/userpanel/MyFrame.java
+++ src/userpanel/MyFrame.java
@@ -5,6 +5,8 @@ import trainset.*;
 import mnist.Mnist;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /**
  *  Made by MarkisJr. 28/07/2020
@@ -12,6 +14,8 @@ import java.awt.*;
 
 public class MyFrame extends JFrame
 {
+	private static final long serialVersionUID = 1L;
+	
 	//Visual components
 	private final JSplitPane splitPane;
 	private final JPanel topPanel;
@@ -28,38 +32,87 @@ public class MyFrame extends JFrame
 		paintArea = new JPanel();
 		output = new JLabel();
 		
-		//Window dimensions and layout
-		setPreferredSize(new Dimension(300, 400));
-		getContentPane().setLayout(new GridLayout());
-		getContentPane().add(splitPane);
-		
 		//Configuring splitpane
 		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
-		splitPane.setDividerLocation(250);
+		splitPane.setDividerLocation(240);
 		splitPane.setTopComponent(topPanel);
 		splitPane.setBottomComponent(bottomPanel);
+		splitPane.setDividerSize(0);
 		
 		//Configuring topPanel
 		topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
 		topPanel.add(paintArea);
+		paintArea.setPreferredSize(new Dimension(240,240));
+		paintArea.setMaximumSize(new Dimension(240,240));
+		paintArea.setBackground(Color.DARK_GRAY);
+		paintArea.addMouseListener(new MouseListener() {
+			
+			volatile private boolean isMouseDown = false;
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				System.out.println("Mouse was pressed...");
+				isMouseDown = true;
+				initThread();
+			}
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				System.out.println("Mouse was released...");
+				isMouseDown = false;
+			}
+			
+			//Unneeded methods required to remain
+			@Override public void mouseExited(MouseEvent e){}
+			@Override public void mouseEntered(MouseEvent e){}
+			@Override public void mouseClicked(MouseEvent e){}
+			
+			//State of thread
+			volatile private boolean isRunning = false;
+			
+			//Checks if there is already a new thread in existence
+			private synchronized boolean checkAndMark()
+			{
+				if (isRunning) 
+					return false;
+				isRunning = true;
+				return true;				
+			}
+			
+			//Created thread and executes code as long as mouse is held down
+			private void initThread()
+			{
+				if (checkAndMark())
+				{
+					new Thread()
+					{
+						public void run()
+						{
+							do 
+							{
+								System.out.println(">><<");
+							}
+							while (isMouseDown);
+							isRunning = false;
+						}
+					}.start();
+				}
+			}
+		});		
 		
 		//Configuring bottomPanel
 		bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.Y_AXIS));
 		bottomPanel.add(output);
 		output.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
-		//Setting font size to be bigger
-		Font labelFont = output.getFont();
-		String labelText = output.getText();
-		int stringWidth = output.getFontMetrics(labelFont).stringWidth(labelText);
-		int componentWidth = output.getWidth();
-		double widthRatio = (double)componentWidth / (double)stringWidth;
-		int newFontSize = (int)(labelFont.getSize() * widthRatio);
-		int componentHeight = output.getHeight();
-		int fontSizeToUse = Math.min(newFontSize, componentHeight);
-		output.setFont(new Font(labelFont.getName(), Font.PLAIN, fontSizeToUse));
+		output.setText("99.9");
 		
-		output.setText("99.9%");
+		//Window dimensions and layout
+		setPreferredSize(new Dimension(250, 400));
+		getContentPane().setLayout(new GridLayout());
+		getContentPane().add(splitPane);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//Packs data for Runnable() class
 		pack();
